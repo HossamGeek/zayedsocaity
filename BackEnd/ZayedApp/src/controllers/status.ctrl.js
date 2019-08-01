45ebc35fe1cd6389a22abe50bf70857c591eb10d
unknown <hossam.mahmoud.azdan@gmail.com>
2019-08-01 17:28:35 +0200

    update stauts

--- BackEnd/ZayedApp/src/controllers/status.ctrl.js
+++ BackEnd/ZayedApp/src/controllers/status.ctrl.js
@@ -1,12 +1,13 @@
 import statusModel from '../models/status.mdl';
 import createService from '../services/create.service';
-import findAllService from '../services/view.service';
+import {findAllService} from '../services/view.service';
+
 
 let errMsg = (err) => {return {data:'Transaction Failed',err:err,success:false}}
 const statusCtrl = {
-    newStatus: function(bdy){createService(statusModel,bdy)},
-    create : function(req,res){
-        this.newStatus(req.body)
+    newStatus: bdy => createService(statusModel,bdy),
+    create : (req,res) => {
+        statusCtrl.newStatus(req.body)
         .then(result=>res.json({data:result,success:true}))
         .catch(err=> res.json(errMsg(err)))
     },
