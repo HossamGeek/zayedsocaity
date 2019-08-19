6ebb08a881da930318e0eef4bc7f5a3a806f2dea
unknown <hossam.mahmoud.azdan@gmail.com>
2019-08-19 17:45:11 +0200

    edit upload files

--- BackEnd/ZayedApp/src/routers/img.route.js
+++ BackEnd/ZayedApp/src/routers/img.route.js
@@ -1,13 +1,34 @@
-import {midParse,express} from '../../config/imports.config';
-import upload from '../services/file-upload.service';
+import {express} from '../../config/imports.config';
+import mediaUpload ,{deleteFile} from '../services/file-upload.service';
 
 const imgRouter = express.Router();
-const singleUpload = upload.any('imgs');
+const mediaUploaded = mediaUpload.any();
 
-imgRouter.post('/issue',(req,res) => {
-   singleUpload(req,res,(err)=>{
-    if(err) return res.json({data:'file upload err',err:err.message,success:false});
-    return res.json({imgUrl:req.files}) 
- })
+
+const mediaIsUploaded = (req,res,next)=>mediaUploaded(req,res,err =>{
+   if(err) return res.json({data:'file upload err',err:err.message,success:false});
+   return res.json({Url:req.files}) 
 })
+
+const multiDelete = (Url,res)=>{
+   let totalFiles = 0,deleting = [], errDeleting = [];
+   Url.map(file=>deleteFile(file.key).then(result=>{
+      totalFiles ++;
+      result.success  ? deleting.push([result.data]) : errDeleting.push([result.data]);
+      if(Url.length == totalFiles)
+         res.json({data:`success Deleting : ${deleting.length}, failed Deleting : ${errDeleting.length}`
+         ,more:{
+            success:(deleting),
+            failed:(errDeleting)
+         },success:true});
+   }))
+}
+
+imgRouter.post('/new',mediaIsUploaded)
+imgRouter.delete('/single/:key',(req,res)=>{
+      deleteFile(req.params.key).then(result=> res.json(result))
+      .catch(err=>res.json(err));                         
+})
+imgRouter.delete('/multi',(req,res)=>{multiDelete(req.body.Url,res)});
+
 module.exports = imgRouter;
--- BackEnd/ZayedApp/src/services/file-upload.service.js
+++ BackEnd/ZayedApp/src/services/file-upload.service.js
@@ -1,4 +1,6 @@
 import {multer,multerS3,IBM} from '../../config/imports.config';
+let videosType = new Set(['video/WebM','video/mp4','video/ogg']);
+let imgsType = new Set(['image/jpeg','image/png','image/gif']);
 
 IBM.config.update ( {
     endpoint: process.env.ibmEndpoint,
@@ -11,14 +13,27 @@ IBM.config.update ( {
     region:"eu-de"
 });
 
-
 const s3 = new IBM.S3();
+
+const bucket = 'appimages';
+
+const storage = multerS3({
+      s3,
+    bucket,
+    acl: 'public-read',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname}); //file.fieldname
+    },
+    key: function (req, file, cb) {
+      cb(null, (Math.floor(Date.now() * Math.random(1))).toString())
+    }
+});
+
 
 const fileFilter = (req,file,cb)=>{
     let fileName = file.fieldname;
     let fileType =file.mimetype;
-    let videosType = new Set(['video/WebM','video/mp4','video/ogg']);
-    let imgsType = new Set(['image/jpeg','image/png']);
+
     if(fileName === 'imgs' && imgsType.has(fileType))
         cb(null,true);
     else if(fileName === 'videos' && videosType.has(fileType))
@@ -27,20 +42,28 @@ const fileFilter = (req,file,cb)=>{
         cb(new Error("invalid type,for imgs only jpeg or png and videos only mp4 or WebM or ogg"),false)    
 }
 
-const upload = multer({
+const mediaUpload = multer({
     fileFilter,
-    storage: multerS3({
-        s3,
-      bucket: 'appimages',
-      acl: 'public-read',
-      metadata: function (req, file, cb) {
-        cb(null, {fieldName: 'TEST_META_DATA'}); //file.fieldname
-      },
-      key: function (req, file, cb) {
-        cb(null, Date.now().toString())
-      }
-    })
+    storage
   })
 
-  export default upload;
+
+
+
+export const deleteFile  =  (key) => {
+    return s3.deleteObject({
+        Bucket: bucket,
+        Key: key
+    }).promise()
+    .then(() =>{
+        return({data:`Item: ${key} deleted!`,success:true});
+    })
+    .catch((err) => {
+        return({data:`Item: ${key} can't deleted!`,err,success:false});
+       
+    });
+}
+
+
+  export default mediaUpload;
   
